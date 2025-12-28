81f75affb1848714e1c307907a2496e5b90d1975
DJJo14 <DJJo14@gmail.com>
2025-12-28 14:15:21 +0000

    setting headers

--- esphome/components/http_request/ota/ota_http_request.cpp
+++ esphome/components/http_request/ota/ota_http_request.cpp
@@ -96,9 +96,9 @@ uint8_t OtaHttpRequestComponent::do_ota_() {
   }
   ESP_LOGVV(TAG, "url_with_auth: %s", url_with_auth.c_str());
   ESP_LOGI(TAG, "Connecting to: %s", this->url_.c_str());
-  ESP_LOGV(TAG, "Request headers:");
+  ESP_LOGI(TAG, "Request headers:");
   for (const auto &header : this->request_headers_) {
-    ESP_LOGV(TAG, "  %s: %s", header.name.c_str(), header.value.c_str());
+    ESP_LOGI(TAG, "  %s: %s", header.name.c_str(), header.value.c_str());
   }
 
   auto container = this->parent_->get(url_with_auth, this->request_headers_);
