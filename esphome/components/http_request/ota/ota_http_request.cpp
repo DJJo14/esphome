231ed7fa14273b3980c4e9fc589d7950a9b75fa3
DJJo14 <DJJo14@gmail.com>
2025-12-28 19:55:49 +0000

    add redirect

--- esphome/components/http_request/ota/ota_http_request.cpp
+++ esphome/components/http_request/ota/ota_http_request.cpp
@@ -101,6 +101,9 @@ uint8_t OtaHttpRequestComponent::do_ota_() {
     ESP_LOGI(TAG, "  %s: %s", header.name.c_str(), header.value.c_str());
   }
 
+  this->parent_->set_follow_redirects(true);
+  this->parent_->set_redirect_limit(10);
+
   auto container = this->parent_->get(url_with_auth, this->request_headers_);
 
   if (container == nullptr || container->status_code != HTTP_STATUS_OK) {
